f5b15b5ec9229ec3bf274e06d20a3c573c97f39d
NingKwok <Ning_Kwok@outlook.com>
2019-02-13 18:06:16 +0800

    Add base Template and error template

--- app.py
+++ app.py
@@ -72,49 +72,22 @@ class Movie(db.Model):
     year = db.Column(db.String(4))
 
 
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
 @app.route('/')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
 
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
 
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html', name=name, movies=movies)
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html'), 404  # 返回模板和状态码
 
 
-# @app.route('/hello')
-# def hello():
-#     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 
-
-# @app.route('/user/<name>')
-# def user_page(name):
-#     return 'User: %s' % name
-
-
-# @app.route('/test')
-# def test_url_for():
-#     print(url_for('hello'))
-#     print(url_for('user_page', name='greyli'))
-#     print(url_for('user_page', name='guoning'))
-#     print(url_for('test_url_for'))
-#     print(url_for('test_url_for', num=2))
-#     return 'Test page'
-
-
-# name = 'Guo Ning'
-# movies = [
-#     {'title': 'My Neighbor Totoro', 'year': '1988'},
-#     {'title': 'Dead Poets Society', 'year': '1989'},
-#     {'title': 'A Perfect World', 'year': '1993'},
-#     {'title': 'Leon', 'year': '1994'},
-#     {'title': 'Mahjong', 'year': '1996'},
-#     {'title': 'Swallowtail Butterfly', 'year': '1996'},
-#     {'title': 'King of Comedy', 'year': '1999'},
-#     {'title': 'Devils on the Doorstep', 'year': '1999'},
-#     {'title': 'WALL-E', 'year': '2008'},
-#     {'title': 'The Pork of Music', 'year': '2012'},
-# ]

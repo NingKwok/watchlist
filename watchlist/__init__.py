c6eca0cf0096edd9ac17a1bb7bfd3c8431dd5d16
NingKwok <Ning_Kwok@outlook.com>
2019-02-22 15:16:35 +0800

    Orignize application with package

---
+++ watchlist/__init__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+# SQLite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev'
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)  # 实例化扩展类
+login_manager = LoginManager(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):  # 创建用户加载回调函数，接受用户 ID 作为参数
+    from watchlist.models import User
+    user = User.query.get(int(user_id))  # 用 ID 作为 User 模型的主键查询对应的用户
+    return user  # 返回用户对象
+
+
+login_manager.login_view = 'login'
+login_manager.login_message = 'LOGIN'
+
+
+@app.context_processor
+def inject_user():
+    from watchlist.models import User
+    user = User.query.first()
+    return dict(user=user)
+
+
+from watchlist import views, errors, commands
---
+++ watchlist/errors.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from flask import render_template
+
+from watchlist import app
+
+
+@app.errorhandler(400)  # 传入要处理的错误代码
+def bad_request(e):  # 接受异常对象作为参数
+    return render_template('errors/400.html'), 400  # 返回模板和状态码
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
